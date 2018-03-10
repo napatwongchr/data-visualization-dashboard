e041af03123ddb631f11afe895e25b1bccbb8aab
Napat Wongcharoen <napat@que.co.th>
2018-03-10 16:22:11 +0700

    draw line to custom chart 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import NavigationBar from './components/common/NavigationBar'
 import ScatterplotChart from './components/Scatterplot'
+import CustomChartExample1 from './components/CustomChartExample1'
 import BarChart from './components/BarChart'
 
 class App extends Component {
@@ -20,16 +21,15 @@ class App extends Component {
               <ScatterplotChart width={500} height={250} />
             </div>
           </div>
-
           <div className='row mt-3'>
             <div className='col border border-default widget-box'>
-              <h4>Map</h4>
+              <CustomChartExample1 width={1000} height={500} />
             </div>
           </div>
-
-          <div className='row mt-3'>
-            <div className='col border border-default widget-box'>
-              <h4>Customchart</h4>
+          <div className='row py-3'>
+            <div className='col border border-default widget-box pt-3'>
+              <h4>Map</h4>
+              <h5>Soon...</h5>
             </div>
           </div>
         </div>
